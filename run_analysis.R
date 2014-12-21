f9e16ba9a8e4b35f9a87c3644dd18472be03c087
cr2003 <crodrigo2003@gmail.com>
2014-12-21 02:02:19 +0100

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,84 @@
+## Author: Carlos Rodrigo
+## Coursera --> Getting and Cleaning Data
+
+## Course Project
+## Assignment Data --> https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+## After decompressing the data you have the directory UCI HAR Dataset with the appropiate file structure
+
+library(dplyr)
+library(magrittr)
+
+
+# Step 1 - Merges the training and the test sets to create one data set.
+
+label <- read.table("UCI HAR Dataset/activity_labels.txt",
+                    stringsAsFactors = FALSE,
+                    col.names = c("y", "activity"))
+
+colnames <- read.table("UCI HAR Dataset/features.txt",
+                       row.names = 1, stringsAsFactors = FALSE,
+                       col.names = c("id", "fnames"))
+colnames <- colnames$fnames
+
+trainid <- read.table("UCI HAR Dataset/train/subject_train.txt",
+                      col.names = "subject")
+
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+names(x_train) <- colnames
+
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt",
+                      col.names = "y")
+
+train <- cbind(trainid, x_train, y_train)
+
+## Test
+testid <- read.table("UCI HAR Dataset/test/subject_test.txt",
+                     col.names = "subject")
+
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+names(x_test) <- colnames
+
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt",
+                     col.names = "y")
+test <- cbind(testid, x_test, y_test)
+
+
+## Binding training and testing
+data <- rbind(train, test)
+
+## Step 2 - Extracts only the measurements on the mean and standard deviation for each measurement.
+meas <- grep("mean\\(|std\\(", names(data), value = TRUE)
+data <- data[, c("subject", "y", meas)]
+
+## Merge the data and label
+data <- inner_join(data, label)
+
+## Step 3 - Uses descriptive activity names to name the activities in the data set
+data <- data[, c("subject", "activity", meas)]
+cnames <- names(data)[-(1:2)]
+
+# step 4 - Appropriately labels the data set with descriptive variable names.
+cnames <- gsub("\\-|\\(|\\)", "", cnames) ## remove parenthesis
+cnames <- gsub("^t", "timedomain", cnames) ## replace "t" by "timedomain"
+cnames <- gsub("^f", "freqdomain", cnames) ## replace "f" by "freqdomain"
+cnames <- gsub("Gyro", "gyroscope", cnames) ## replace "Gyro" by "gyroscope"
+cnames <- gsub("Acc", "accelerometer", cnames) ## replace "Acc" by "accelerometer"
+cnames <- gsub("Mag", "magnitude", cnames) ## replace "Mag" by "magnitude"
+cnames <- gsub("BodyBody", "body", cnames) ## Remove duplicate body
+cnames <- gsub("(.+)(std|mean)(X$|Y$|Z$)", "\\1\\3\\2", cnames) ## Put std or mean at the end
+cnames <- tolower(cnames)
+names(data)[-(1:2)] <- cnames
+
+# step 5 - From the data set in step 4, creates a second, independent tidy data set with the average 
+## of each variable for each activity and each subject.
+
+data_agg <- data %>%
+        group_by(subject, activity) %>%
+        summarise_each(funs(mean))
+
+## Save the tidy datasets
+write.table(data_agg, "tidydata.txt", row.name = FALSE)
+
+
+
